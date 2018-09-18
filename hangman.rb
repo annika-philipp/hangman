fdd38fcaf166cf55f59988ce81624f5d40e92206
Annika Philipp <liebste.anni@gmail.com>
2018-09-19 10:57:12 +1200

    refactors mask_letters and letter_in_answer methods

--- hangman.rb
+++ hangman.rb
@@ -12,12 +12,13 @@ class HangmanGame
     @wrong_letters_guessed = []
     @lives = 8
 
+    puts "Hello, time to play hangman"
+
     while !game_over? do
       mask_letters
       letter_in_answer(user_input)
     end
 
-    # play_again
   end
 
   def user_input
@@ -28,20 +29,32 @@ class HangmanGame
     gets.chomp.downcase[0,1]
   end
 
+  def valid_answer?(answer)
+    answer.match(/\p{L}\p{M}*+/)
+  end
+
+  def duplicate_letter(answer)
+    if @wrong_letters_guessed.include?(answer) || @correct_letters_guessed.include?(answer)
+      puts "You've already guessed this letter, try again"
+      true
+    end
+  end
+
+  def new_letter(answer)
+    if word_to_guess.include? answer
+      puts "This letter is in the word"
+      @correct_letters_guessed.push(answer)
+      @correct_letters_guessed.push(answer.upcase)
+    else
+      puts "This letter is not in the word"
+      @wrong_letters_guessed.push(answer)
+      reduce_life
+    end
+  end
+
   def letter_in_answer(answer)
-    # break into two methods
-    if answer.match(/\p{L}\p{M}*+/)
-      if @wrong_letters_guessed.include?(answer) || @correct_letters_guessed.include?(answer)
-        puts "You've already guessed this letter, try again"
-      elsif word_to_guess.include? answer
-        puts "This letter is in the word"
-        @correct_letters_guessed.push(answer)
-        @correct_letters_guessed.push(answer.upcase)
-      else
-        puts "This letter is not in the word"
-        @wrong_letters_guessed.push(answer)
-        reduce_life
-      end
+    if valid_answer?(answer)
+      new_letter(answer) unless duplicate_letter(answer)
     else
       puts "Please enter a valid letter"
     end
@@ -52,20 +65,10 @@ class HangmanGame
   end
 
   def mask_letters
-    # use filter or reduce instead
-    puts "WTD: #@word_to_display"
-    @hidden_word = word_to_display.filter do |letter|
-      letter.include? answer || "_"
-      end 
+    @hidden_word = word_to_display.map do |letter| 
+      @correct_letters_guessed.include?(letter) ? letter : "_" 
+    end
     puts @hidden_word.join(" ")
-    # @hidden_word = word_to_display.map do |letter|
-    #   if @correct_letters_guessed.include? letter
-    #     letter
-    #   else
-    #     "_"
-    #   end 
-    # end
-    # puts @hidden_word.join(" ")
   end
 
   def game_over?
@@ -86,9 +89,7 @@ class HangmanGame
     end
   end
 
-  puts "Hello, time to play hangman"
-
 end
 
-h = HangmanGame.new("Pizza")
+h = HangmanGame.new("PizZa")
 h.play_game
